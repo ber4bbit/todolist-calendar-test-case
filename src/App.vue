7917eb783d1b807a1b26aca641dfe2b3e32f1114
Egor Mikhaylov <ber4bbit@gmail.com>
2025-04-22 23:20:32 +0700

    init and configure project

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import {ref} from "vue";
+import {CalendarDate} from '@internationalized/date'
+
+const value = ref(new CalendarDate(2025, 4, 21));
+const modalRef = ref<boolean>(false);
+
+const onClickDate = () => {
+	modalRef.value = true;
+	console.log('change')
+}
+</script>
+
+<template>
+	<UApp>
+		<UModal v-model:open="modalRef" />
+		<UCalendar
+			size="xl"
+			@update="onClickDate"
+		/>
+	</UApp>
+</template>
+
+<style scoped></style>
